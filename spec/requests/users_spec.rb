5c85b751b20c82e3c33126ad4d85fd16d5815d0b
John Doe <slink44@hotmail.com>
2011-03-29 21:56:05 +0100

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,21 +19,44 @@ describe "Users" do
         end.should_not change(User, :count)
       end
     end
-    
+    #Added this test so if email was used in Web interface, this test would still work. #{Time.now.nsec}
     describe "success" do
 
       it "should make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",         :with => "Example User1"
-          fill_in "Email",        :with => "user@example1.com"
-          fill_in "Password",     :with => "foobar1"
-          fill_in "Confirmation", :with => "foobar1"
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
           click_button
           response.should have_selector("div.flash.success",
                                         :content => "Welcome")
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
+      end
+    end
+  end
+  
+  describe "sign in/out" do
+  
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email,    :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+  
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        integration_sign_in(user)
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
       end
     end
   end
